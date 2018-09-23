9852b58bac08189c0f96c7989abfdc0bb344d579
Gagandeep Singh <gagandeepsingh25@outlook.com>
2018-09-23 18:52:51 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const app = express();
 
-
 app.set("view engine", "pug");
 app.set("views", "./views");
 app.use(express.static('resources'))
 app.get('/', (req, res)=> res.render("main"));
 
+var ip = process.env.IP || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
 
-app.listen(8080, console.log("Server running on 8080 port"));
+
+app.listen(8080, ip);
