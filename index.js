3a57c8463ea495568e1b60a3871dc8e086daea0c
Gagandeep Singh <gagandeepsingh25@outlook.com>
2018-09-26 11:16:56 +0530

    Update index.js

--- index.js
+++ index.js
@@ -8,4 +8,4 @@ app.use(express.static('resources'))
 app.get('/', (req, res)=> res.render("main"));
 
 
-app.listen(3000, console.log("Server running on 3000 port"));
+app.listen(3000, console.log("Running on port 3000"));
